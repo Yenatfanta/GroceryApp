1ad8fb36e64e0c8182c91a19c52f5c9485089146
yenatfanta <Yenatkasu@gmail.com>
2025-02-21 13:54:16 -0500

    Initial commit

---
+++ GroceryApp/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  GroceryApp
+//
+//  Created by Yenat Feyyisa on 2/21/25.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Yenat Feyyisa on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
